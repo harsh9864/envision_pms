d4d46bd18de4bc17787eea91ea3d9ec301869933
Yogesh <yogesh@sanskartechnolab.com>
2024-10-09 17:20:25 +0530

    calculate remaining hr on the row added in the child table

--- envision_pms/public/js/timesheet.js
+++ envision_pms/public/js/timesheet.js
@@ -1,50 +1,37 @@
 frappe.ui.form.on("Timesheet Detail", {
-  //   Change in the sales order
-  //   custom_remaining_hrs
   task: function (frm, cdt, cdn) {
     let current_row = locals[cdt][cdn];
-    // console.log("current_row", current_row);
     console.log("Task", current_row.task);
-
-    frappe.call({
-      method:
-        "envision_pms.py.calculate_remaining_hrs.calculate_task_remaining_hrs",
-      args: {
-        task: current_row.task,
-      },
-      callback: function (r) {
-        if (!r.exc) {
-          // Refresh the child table after adding rows
-          console.log("Entire",r.message);
-          remaining_hrs = r.message.remaining_hrs || 0;
-          expected_hrs = r.message.expected_hrs || 0;
-          console.log(expected_hrs  );
-          // current_row.custom_remaining_hrs = remaining_hrs;
-          frappe.model.set_value(
-            cdt,
-            cdn,
-            "custom_remaining_hrs",
-            remaining_hrs
-          );
-
-          frappe.model.set_value(cdt, cdn, "expected_hours", expected_hrs);
-        }
-      },
-    });
+    if (current_row.task) {
+      calculate_remaining_hrs(cdt, cdn, current_row.task);
+    }
   },
 
-  //   customer: function (frm) {
-  //     frm.set_query("custom_sales_orders", function () {
-  //       return {
-  //         filters: {
-  //           customer: frm.doc.customer,
-  //         },
-  //       };
-  //     });
-  //   },
+  time_logs_add: function (frm, cdt, cdn) {
+    let current_row = locals[cdt][cdn];
+    console.log("Task", current_row.task);
+    if (current_row.task) {
+      calculate_remaining_hrs(cdt, cdn, current_row.task);
+    }
+  },
 });
 
-// function calculate_remaining_hrs(frm,cdt,cdn){
-//     current_row = locals[cdt][cdn];
-
-// }
+// Function to calculate remaining hours
+function calculate_remaining_hrs(cdt, cdn, task) {
+  frappe.call({
+    method:
+      "envision_pms.py.calculate_remaining_hrs.calculate_task_remaining_hrs",
+    args: {
+      task: task,
+    },
+    callback: function (r) {
+      if (!r.exc) {
+        console.log("Entire", r.message);
+        let remaining_hrs = r.message.remaining_hrs || 0;
+        let expected_hrs = r.message.expected_hrs || 0;
+        frappe.model.set_value(cdt, cdn, "custom_remaining_hrs", remaining_hrs);
+        frappe.model.set_value(cdt, cdn, "expected_hours", expected_hrs);
+      }
+    },
+  });
+}
